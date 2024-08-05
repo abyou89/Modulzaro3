8a1ba30732eaf51a78fa0af49fd00a9447e158d3
younes <younes.abdalla@gmail.com>
2024-08-05 22:44:13 +0200

    Mindent tudo gep - Zene elkeszitve

--- src/modell/MindentTudoGep.java
+++ src/modell/MindentTudoGep.java
@@ -3,6 +3,8 @@ package modell;
 import java.util.Arrays;
 
 public class MindentTudoGep {
+    
+    public enum Zenek{SZOMORU, VIDAM, BANATOS};
 
     public static final int LOTTO_DB = 5;
     public static final int TOTO_DB = 14;
@@ -10,15 +12,15 @@ public class MindentTudoGep {
     private int[] lotto;
     private String[] totoEredmeny;
     private int[] totoMerkozes;
-    //private String[] totoMerkozes = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "13+1"};
     private String[] kimenetel = {"1", "2", "X"};
+    private Zenek zene;
 
     public MindentTudoGep() {
 
         lotto = new int[LOTTO_DB];
         totoEredmeny = new String[TOTO_DB];
         totoMerkozes = new int[TOTO_DB - 1];
-
+        
     }
 
     public void setLotto() {
@@ -29,7 +31,7 @@ public class MindentTudoGep {
 
     }
 
-    public void getLotto() {
+    public void kiirLotto() {
 
         for (int i = 0; i < LOTTO_DB; i++) {
             System.out.print(lotto[i] + ", ");
@@ -43,7 +45,7 @@ public class MindentTudoGep {
         }
     }
 
-    public void getTotoMerkozes() {
+    public void kiirTotoMerkozes() {
         for (int i = 0; i < TOTO_DB - 1; i++) {
             System.out.printf("%3d", totoMerkozes[i]);
         }
@@ -56,10 +58,20 @@ public class MindentTudoGep {
         }
     }
 
-    public void getToto() {
+    public void kiirToto() {
         for (int i = 0; i < TOTO_DB; i++) {
             System.out.printf("%3s", totoEredmeny[i]);
         }
+        System.out.println("");
+    }
+    
+    public void setZene(Zenek zene){
+        this.zene = zene;
+    }
+    
+    public String zeneValasztas(){
+        
+        return "valasztott zene: " + zene;
     }
 
 }
